3d681a0b3d7158b6b3bc9ed88c73ac46f1450f11
Michael Ihle <mihle@genability.com>
2014-04-03 12:06:45 -0700

    Added new account methods to client library

--- src/main/java/com/genability/client/api/service/AccountService.java
+++ src/main/java/com/genability/client/api/service/AccountService.java
@@ -1,22 +1,38 @@
 package com.genability.client.api.service;
 
+import java.text.MessageFormat;
+
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.genability.client.types.Account;
-import com.genability.client.types.Response;
 import com.genability.client.api.request.DeleteAccountRequest;
-import com.genability.client.api.request.GetAccountsRequest;
+import com.genability.client.api.request.GetAccountRatesRequest;
 import com.genability.client.api.request.GetAccountRequest;
+import com.genability.client.api.request.GetAccountTariffsRequest;
+import com.genability.client.api.request.GetAccountsRequest;
+import com.genability.client.types.Account;
+import com.genability.client.types.PropertyData;
+import com.genability.client.types.Response;
+import com.genability.client.types.Tariff;
+import com.genability.client.types.TariffRate;
 
 public class AccountService extends BaseService {
 
     private static final TypeReference<Response<Account>> ACCOUNT_RESPONSE_TYPEREF = new TypeReference<Response<Account>>() {};
+
+	private static final TypeReference<Response<PropertyData>> PROPERTY_DATA_RESPONSE_TYPEREF = new TypeReference<Response<PropertyData>>() {
+	};
 	
+	private static final TypeReference<Response<TariffRate>> TARIFF_RATE_RESPONSE_TYPEREF = new TypeReference<Response<TariffRate>>() {
+	};
+
+	private static final TypeReference<Response<Tariff>> TARIFF_RESPONSE_TYPEREF = new TypeReference<Response<Tariff>>() {
+	};
+
 	public Response<Account> getAccounts(GetAccountsRequest request)
 	{
 		if(log.isDebugEnabled()) log.debug("getAccounts called");
 		
 		Response<Account> response = this.callGet(
-				"beta/accounts", 
+"v1/accounts",
 				request.getQueryParams(),
 				ACCOUNT_RESPONSE_TYPEREF);
 		
@@ -31,7 +47,7 @@ public class AccountService extends BaseService {
 
 		if(log.isDebugEnabled()) log.debug("getAccount called");
 
-		String uri = "beta/accounts";
+		String uri = "v1/accounts";
 		if (request.getAccountId() != null && request.getAccountId().length() !=0) {
 			uri += "/" + request.getAccountId();
 		}
@@ -48,13 +64,54 @@ public class AccountService extends BaseService {
 	}
 	
 	
+	public Response<Account> deleteAccount(DeleteAccountRequest request) {
+
+		if (log.isDebugEnabled())
+			log.debug("deleteAccount called");
+
+		String uri = "v1/accounts";
+		if (request.getAccountId() != null
+				&& request.getAccountId().length() != 0) {
+			uri += "/" + request.getAccountId();
+		}
+
+		Response<Account> response = this.callDelete(uri,
+				request.getQueryParams(), ACCOUNT_RESPONSE_TYPEREF);
+		if (log.isDebugEnabled())
+			log.debug("deleteAccount completed");
+
+		return response;
+
+	}
+
+	public Response<PropertyData> getAccountProperties(GetAccountRequest request) {
+
+		if (log.isDebugEnabled())
+			log.debug("getAccountProperties called");
+
+		String uri = "v1/accounts";
+		if (request.getAccountId() != null
+				&& request.getAccountId().length() != 0) {
+			uri += "/" + request.getAccountId();
+			uri += "/properties";
+		}
+
+		Response<PropertyData> response = this.callGet(uri,
+				request.getQueryParams(), PROPERTY_DATA_RESPONSE_TYPEREF);
+
+		if (log.isDebugEnabled())
+			log.debug("getAccountProperties completed");
+
+		return response;
+
+	}
+
 	public Response<Account> addAccount(Account account) {
 		
 
 		if(log.isDebugEnabled()) log.debug("addAccount called");
 	
-		Response<Account> response = this.callPost(
-				"beta/accounts", 
+		Response<Account> response = this.callPost("v1/accounts",
 				account,
 				ACCOUNT_RESPONSE_TYPEREF);
 		
@@ -64,25 +121,93 @@ public class AccountService extends BaseService {
 		
 	}
 
-	public Response<Account> deleteAccount(DeleteAccountRequest request) {
-		
+	public Response<Account> updateAccount(Account account) {
 
-		if(log.isDebugEnabled()) log.debug("deleteAccount called");
-	
-		String uri = "beta/accounts";
-		if (request.getAccountId() != null && request.getAccountId().length() !=0) {
-			uri += "/" + request.getAccountId();
-		}		
-		
-		Response<Account> response = this.callDelete(
-				uri,
-				request.getQueryParams(),
+		if (log.isDebugEnabled())
+			log.debug("updateAccount called");
+
+		String uri = "v1/accounts";
+
+		if (account.getAccountId() != null
+				&& account.getAccountId().length() != 0) {
+			uri += "/" + account.getAccountId();
+		}
+
+		Response<Account> response = this.callPut(uri, account,
 				ACCOUNT_RESPONSE_TYPEREF);
-		if(log.isDebugEnabled()) log.debug("deleteAccount completed");
-		
+
+		if (log.isDebugEnabled())
+			log.debug("updateAccount completed");
+
 		return response;
-		
+
 	}
 	
-	
+	public Response<PropertyData> interviewAccount(String accountId,
+			PropertyData interviewAnswer) {
+
+		if (log.isDebugEnabled())
+			log.debug("interviewAccount called");
+
+		String uri = "v1/accounts/{0}/interview";
+
+		if (accountId != null && accountId.length() != 0) {
+			uri = MessageFormat.format(uri, accountId);
+		}
+
+		Response<PropertyData> response = this.callPut(uri, interviewAnswer,
+				PROPERTY_DATA_RESPONSE_TYPEREF);
+
+		if (log.isDebugEnabled())
+			log.debug("interviewAccount completed");
+
+		return response;
+
+	}
+
+	public Response<TariffRate> getAccountRates(GetAccountRatesRequest request) {
+
+		if (log.isDebugEnabled())
+			log.debug("getAccountRates called");
+
+		String uri = "v1/accounts/{0}/rates";
+
+		if (request.getAccountId() != null
+				&& request.getAccountId().length() != 0) {
+			uri = MessageFormat.format(uri, request.getAccountId());
+		}
+
+		Response<TariffRate> response = this.callGet(uri,
+				request.getQueryParams(), TARIFF_RATE_RESPONSE_TYPEREF);
+
+		if (log.isDebugEnabled())
+			log.debug("getAccountRates completed");
+
+		return response;
+
+	}
+
+	public Response<Tariff> getAccountTariffs(GetAccountTariffsRequest request) {
+
+		if (log.isDebugEnabled())
+			log.debug("getAccountTariffs called");
+
+		String uri = "v1/accounts/{0}/tariffs";
+
+		if (request.getAccountId() != null
+				&& request.getAccountId().length() != 0) {
+			uri = MessageFormat.format(uri, request.getAccountId());
+		}
+
+		Response<Tariff> response = this.callGet(uri, request.getQueryParams(),
+				TARIFF_RESPONSE_TYPEREF);
+
+		if (log.isDebugEnabled())
+			log.debug("getAccountTariffs completed");
+
+		return response;
+
+	}
+
+
 }
