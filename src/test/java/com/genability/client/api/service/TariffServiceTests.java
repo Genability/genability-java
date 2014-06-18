9b034b71d7ea2de97b6bb3b72001227a5ba426b7
Michael Ihle <mihle@genability.com>
2014-06-18 12:02:58 -0700

    Added getTariff, Australian tariff check, and check for currency code

--- src/test/java/com/genability/client/api/service/TariffServiceTests.java
+++ src/test/java/com/genability/client/api/service/TariffServiceTests.java
@@ -1,23 +1,35 @@
 package com.genability.client.api.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import com.genability.client.api.request.GetTariffRequest;
+import com.genability.client.api.request.GetTariffsRequest;
 import com.genability.client.types.CustomerClass;
 import com.genability.client.types.Response;
 import com.genability.client.types.SortOrder;
 import com.genability.client.types.Tariff;
 import com.genability.client.types.TariffType;
-import com.genability.client.api.request.GetTariffsRequest;
-import com.genability.client.api.service.TariffService;
-
-import org.joda.time.DateTime;
-import org.junit.Test;
 
 public class TariffServiceTests extends BaseServiceTests {
 
 	private static TariffService tariffService = genabilityClient.getTariffService();
 
 	
+	@Test
+	public void testGetTariff() {
+
+		String testCase = "Case 1 - call get Australian tariff";
+		GetTariffRequest request = new GetTariffRequest();
+		request.setMasterTariffId(3183808l);
+		callGetTariff(testCase, request);
+
+	}
+
 	@Test
 	public void testGetTariffs() {
 		
@@ -34,11 +46,11 @@ public class TariffServiceTests extends BaseServiceTests {
 		request.setPopulateProperties(Boolean.TRUE);
 		callGetTariffs(testCase, request);
 		
-		testCase = "Case 3 - explicit effective on";
+		testCase = "Case 4 - explicit effective on";
 		request.setEffectiveOn(new DateTime(DateTime.now().getYear()-1,11,11,1,0,0,0));
 		callGetTariffs(testCase, request);
 		
-		testCase = "Case 4 - common scenario, active residential tariffs for a zipcode";
+		testCase = "Case 5 - common scenario, active residential tariffs for a zipcode";
 		request = new GetTariffsRequest();
 		request.setCustomerClasses(CustomerClass.RESIDENTIAL);
 		request.setTariffTypes(TariffType.DEFAULT, TariffType.ALTERNATIVE);
@@ -66,10 +78,30 @@ public class TariffServiceTests extends BaseServiceTests {
 			
 			assertNotNull("tariffId null",tariff.getTariffId());
 			assertNotNull("effectiveDate", tariff.getEffectiveDate());
+			assertNotNull("currency", tariff.getCurrency());
 			
 		}
 		
 	}
 	
+	public void callGetTariff(String testCase, GetTariffRequest request) {
+
+		Response<Tariff> restResponse = tariffService.getTariff(request);
+
+		assertNotNull("restResponse null", restResponse);
+		assertEquals("bad status", restResponse.getStatus(),
+				Response.STATUS_SUCCESS);
+		assertEquals("bad type", restResponse.getType(), Tariff.REST_TYPE);
+		assertTrue("bad count", restResponse.getCount() > 0);
+
+		for (Tariff tariff : restResponse.getResults()) {
+
+			assertNotNull("tariffId null", tariff.getTariffId());
+			assertNotNull("effectiveDate", tariff.getEffectiveDate());
+			assertNotNull("currency", tariff.getCurrency());
+
+		}
+
+	}
 
 }
