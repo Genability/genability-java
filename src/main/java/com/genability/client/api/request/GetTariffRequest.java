fa2136f3c5fd74a1037c2782a80b6dd7277ceb37
Jason Riley <jriley@genability.com>
2014-08-26 12:47:44 -0700

    new get tariff request arguments (aug 14 release)

--- src/main/java/com/genability/client/api/request/GetTariffRequest.java
+++ src/main/java/com/genability/client/api/request/GetTariffRequest.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.apache.http.NameValuePair;
+import org.joda.time.DateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -17,11 +18,50 @@ public class GetTariffRequest extends AbstractRequest implements Serializable {
 	
 	private Long masterTariffId;
 	
+	/**
+	 * Private member variable for accountId
+	 */
 	private String accountId;
 	
+	/**
+	 * Private member variable for PopulateProperties.
+	 */
 	private Boolean populateProperties;
 	
+	/**
+	 * Private member variable for PopulateRates.
+	 */
 	private Boolean populateRates;
+
+	/**
+	 * Private member variable for PopulateDocuments.
+	 */
+	private Boolean populateDocuments;
+
+	/**
+	 * Private member variable for EffectiveOn.
+	 */
+	private DateTime effectiveOn;
+
+	/**
+	 * Private member variable for applicableRatesOnly.
+	 */
+	private Boolean applicableRatesOnly;
+
+	/**
+	 * Private member variable for applyContractedRates.
+	 */
+	private Boolean applyContractedRates;
+	
+	/**
+	 * Private member variable for lookupVariableRates.
+	 */
+	private Boolean lookupVariableRates;
+	
+	/**
+	 * Private member variable for bundleRates.
+	 */
+	private Boolean bundleRates;
 	
 
 	public Long getMasterTariffId() {
@@ -64,6 +104,66 @@ public class GetTariffRequest extends AbstractRequest implements Serializable {
 	}
 
 
+	public Boolean getPopulateDocuments() {
+		return populateDocuments;
+	}
+
+
+	public void setPopulateDocuments(Boolean populateDocuments) {
+		this.populateDocuments = populateDocuments;
+	}
+
+
+	public DateTime getEffectiveOn() {
+		return effectiveOn;
+	}
+
+
+	public void setEffectiveOn(DateTime effectiveOn) {
+		this.effectiveOn = effectiveOn;
+	}
+
+
+	public Boolean getApplicableRatesOnly() {
+		return applicableRatesOnly;
+	}
+
+
+	public void setApplicableRatesOnly(Boolean applicableRatesOnly) {
+		this.applicableRatesOnly = applicableRatesOnly;
+	}
+
+
+	public Boolean getApplyContractedRates() {
+		return applyContractedRates;
+	}
+
+
+	public void setApplyContractedRates(Boolean applyContractedRates) {
+		this.applyContractedRates = applyContractedRates;
+	}
+
+
+	public Boolean getLookupVariableRates() {
+		return lookupVariableRates;
+	}
+
+
+	public void setLookupVariableRates(Boolean lookupVariableRates) {
+		this.lookupVariableRates = lookupVariableRates;
+	}
+
+
+	public Boolean getBundleRates() {
+		return bundleRates;
+	}
+
+
+	public void setBundleRates(Boolean bundleRates) {
+		this.bundleRates = bundleRates;
+	}
+
+
 	@Override
 	@JsonIgnore
 	public List<NameValuePair> getQueryParams() {
@@ -74,6 +174,12 @@ public class GetTariffRequest extends AbstractRequest implements Serializable {
 		addParam(qparams,"accountId",accountId);
 		addParam(qparams,"populateProperties",populateProperties);
 		addParam(qparams,"populateRates",populateRates);
+		addParam(qparams,"populateDocuments",populateDocuments);
+		addParam(qparams,"effectiveOn",effectiveOn);
+		addParam(qparams,"applicableRatesOnly",applicableRatesOnly);
+		addParam(qparams,"applyContractedRates",applyContractedRates);
+		addParam(qparams,"lookupVariableRates",lookupVariableRates);
+		addParam(qparams,"bundleRates",bundleRates);
 		
 		return qparams;
 		
